3e7087ed1b5e39ad945545fc18c47da44f98b7c2
Omer Shaked <shakedomer@gmail.com>
2012-07-17 17:37:21 +0300

    Initial Commit

---
+++ stereo_vision/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  stereo_vision
+//
+//  Created by Omer Shaked on 7/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ stereo_vision/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  stereo_vision
+//
+//  Created by Omer Shaked on 7/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
